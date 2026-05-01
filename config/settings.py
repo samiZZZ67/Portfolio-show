2ef339840e8b4e3b4f8fbc3744ed77e870fcf61f
samiZZZ67 <abiyafikre@gmail.com>
2026-05-01 15:10:57 +0300

    add

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "portfolio-show.onrender.com").split(",")
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
